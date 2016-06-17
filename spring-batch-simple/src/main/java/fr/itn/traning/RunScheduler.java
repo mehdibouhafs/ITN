fd0a0587cf0fac1a921f4b1a9359c94abbefe382
Bouhafs <mehdibouhafs@hotmail.fr>
2016-06-17 16:49:00 +0200

    CRON Example and Correction BUGS

--- spring-batch-simple/src/main/java/fr/itn/traning/RunScheduler.java
+++ spring-batch-simple/src/main/java/fr/itn/traning/RunScheduler.java
@@ -2,4 +2,11 @@ package fr.itn.traning;
 
 public class RunScheduler {
 	
+	public void run(){
+		try{
+			System.out.println("Merci beaucoup va se répeter toutes le 5 secondes exemple cron");
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
 }
